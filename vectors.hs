8e3c1eeb397f24c76572f6698f8140539d73610d
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-04-23 10:47:11 -0300

    Adds first (multi)?dimensional array example

---
+++ vectors.hs
@@ -0,0 +1,7 @@
+import Data.Array
+
+-- Single line array
+ary1 = array (1,4) [(1,'A'), (2,'B'), (3,'C'), (4,'D')]
+
+-- matrix 2x2
+ary2 = array ((1,1), (2,2)) [((1,1),'A'), ((1,2),'B'), ((2,1),'C'), ((2,2),'D')]
