afbaf93549ecb1f43bd71a8a00b6d45762171e11
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-08-08 00:25:56 +0200

    [WIP] Adding weight to edges and some extra functions

--- graph.hs
+++ graph.hs
@@ -1,23 +1,30 @@
 type Vertex = Int
--- TODO? ^^
---type Weight = Int
---type Edge = (Vertex, Vertex, Weight)
-type Edge = (Vertex, Vertex)
+type Weight = Int
+type Edge = ((Vertex, Vertex), Weight)
 
 type Graph = [Edge]
 
 graph :: [Edge]
 graph = [
-          (1,2), (1,3), (1,4), (1,5),
-          (2,6), (2,7),
-          (4,8),
-          (5,9)
+          ((1,2),1), ((1,3),2), ((1,4),3), ((1,5),4),
+          ((2,6),6), ((2,7),7),
+          ((4,8),5),
+          ((5,9),4)
         ]
 
-adjacents :: Graph -> Vertex -> [Vertex]
+adjacents :: Graph -> Vertex -> [(Vertex, Weight)]
 adjacents [] _ = []
-adjacents ((a,b):c) v | (a == v) = b:(adjacents c v)
-                      | (b == v) = a:(adjacents c v)
-                      | otherwise = adjacents c v
+adjacents (((a,b),w):c) v | (a == v) = (b,w):(adjacents c v)
+                          | (b == v) = (a,w):(adjacents c v)
+                          | otherwise = adjacents c v
 
 
+pathExists :: Graph -> Vertex -> Vertex -> Bool
+pathExists g v1 v2  | belongs_to (adjacents g v1) v2 == True = True
+                    | otherwise = [ pathExists g v2 x | (x,w) <- adjacents g v2 ]
+
+
+belongs_to :: [(Vertex, t)] -> Vertex -> Bool
+belongs_to [] _ = False
+belongs_to ((v,w):xs) e | (v == e) = True
+                        | otherwise = belongs_to xs e
