f5f8fc23df2dc96d5a874fcd056c982a04b6de05
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-09-08 08:32:49 +0200

    Fixes test for zero

--- zero.hs
+++ zero.hs
@@ -1,3 +1,17 @@
+-- This tests wheter the machine thinks it reached zero.
 zero :: (Ord t, Fractional t) => t -> t
-zero n  | (n > 0) = zero n/2
+zero n  | (n + 1 > 1) = zero (n/2)
         | otherwise = n
+
+
+-- Compare to the C program:
+-- #include <stdio.h>
+--
+-- int main () {
+--   double i = 1.0;
+--   while (i + 1 > 1) {
+--     i /= 2.0;
+--   }
+--   printf("I think %1.70f is 0\n", i);
+--   return 0;
+-- }
