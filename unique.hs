01dd0927a530c8938b8adf37be78060f63217ebd
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-12-12 12:26:39 +0000

    Adds Data.Unique example

---
+++ unique.hs
@@ -0,0 +1,11 @@
+import Data.Unique
+
+main = do
+      unique <- newUnique
+      print $ hashUnique unique
+
+      unique <- newUnique
+      print (hashUnique unique)
+
+      unique <- newUnique
+      print $ hashUnique unique
