32b54c8c6e5adb37cceaff0405bfd0174de0d1a8
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-12-31 14:59:52 +0000

    Adds extra type definitions

--- types.hs
+++ types.hs
@@ -1,7 +1,15 @@
 type Name = String
 type Age = Int
 type Language = String
-type Person = (Name, Age, Language)
+type Person = (Name, Age, Language) -- be aware, this is an alias for a tuple
+
+data Compass = North | East | South | West
+    deriving (Eq, Show, Enum, Ord) -- now you can print, compare and do more stuff with this data
+
+data Expression = Number Int
+                | Add Expression Expression
+                | Subtract Expression Expression
+                deriving (Eq, Ord, Show)
 
 person :: Person
 
