a63addeb7f40fb4bb71d766a0fb5f906100e3561
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2018-07-01 19:40:38 +0200

    Add caesar cipher

---
+++ caesar.hs
@@ -0,0 +1,8 @@
+import Data.Char
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
